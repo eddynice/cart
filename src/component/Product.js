e07595097f7f967eafdd9bce9952d821223f6c34
eddynice <39999238+eddynice@users.noreply.github.com>
2021-04-01 01:57:06 +0000

    Change from incart to buy

--- src/component/Product.js
+++ src/component/Product.js
@@ -32,7 +32,7 @@ export default class Product extends Component {
               >  incart{inCart ? (
                  <p className="text-capitalize mb-0" disabled>
                      {" "} 
-                     in ancart
+                     Buy
                  </p>
              
              ):(
